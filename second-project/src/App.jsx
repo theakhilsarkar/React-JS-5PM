90abb799a24bd7a581f0396c6ee5eb13d76a022f
theakhilsarkar <akhilsodvadiya7@gmail.com>
2026-02-23 18:10:44 +0530

    ...

--- second-project/src/App.jsx
+++ second-project/src/App.jsx
@@ -1,35 +1,26 @@
+import { products } from './products.js'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
-
-function App() {
+import "./App.css";
+const App = () => {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <>
+    <h1>{count}</h1>
+    <button onClick={() => {
+      setCount(count + 1)
+    }}>++</button>
+  </>
 }
 
-export default App
+export default App;
+
+// state - state is one type of data which render in ui and also can change with UI.
+
+// state is something which can hold data which can used to render in ui.
+
+// we cant change state directly by assignment.
+// we have to use update function to set state.
+
+// useState = Hook
+// useState is a hook of react which is used to create state.
+
