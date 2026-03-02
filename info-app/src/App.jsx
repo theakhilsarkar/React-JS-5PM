db0307750dcb359c654ba11b5722e9b0fca346e4
theakhilsarkar <akhilsodvadiya7@gmail.com>
2026-03-02 17:26:49 +0530

    ...

--- info-app/src/App.jsx
+++ info-app/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import Footer from './components/Footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <>
+    <Navbar />
+    <Hero />
+    <Footer />
+  </>
 }
 
-export default App
+export default App;
+
+
+// folder structure
+// informative website
+// src/
+// src/components/
+// src/components/Navbar/
+// src/components/Navbar/Navbar.jsx
+// src/components/Navbar/Navbar.css
+
+// multi-page,screen website
+
+// src/
+// src/screens/
+// src/screens/Home
+// src/screens/Home/Home.jsx
+// src/screens/Home/Home.css
+
+// src/components/
+// src/components/Form/Form.jsx,Form.css
+// src/components/Navbar/Navbar.jsx,Navbar.css
+// src/components/Button/Button.jsx,Button.css
