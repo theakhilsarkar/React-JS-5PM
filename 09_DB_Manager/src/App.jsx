94e72b6a162f41fc43aaa382a6d033879841559e
theakhilsarkar <akhilsodvadiya7@gmail.com>
2026-04-24 18:16:11 +0530

    ....

---
+++ 09_DB_Manager/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+
+// HRMS - 
+// employee management system 
+
+// admin
+// manager
+// employee
+
+// HRMS
+// 1. role based authentication - login,logout 
+// 2. admin - USER(CRUD)
+// 3. manager - See All Employee
+// 4. employee - see own profile
+// 5. daily attendance - check in, check out
+// 6. hourly based salary calculation and salary slip
+
+// 8hrs
+// in time -  9:00 am 
+// out time - 5:00 pm 
+
+// 9:45 -> 4:45 -> 7hr, 
+
+// 1 paid leave - free leave - no salary deduction
+// more than 1 paid leave not allow, charges apply 
+// sunday leave - no count of sunday
+
+// leave apply 
+// absent - deduction 
